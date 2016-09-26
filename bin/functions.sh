9cc9c760382af2e3d0415586390ec5687b5b09c3
Lucas Bremgartner <lucas.bremgartner@swisscom.com>
2016-09-26 13:07:45 +0200

    debugging out redirect

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail # Enable bash strict mode (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+STDOUT=/dev/stdout
 if [[ ${DEBUG:-0} -eq 1 ]]; then
 	set -x # DEBUG
+	STDOUT=/dev/null
 fi
 
 steptxt="----->"
